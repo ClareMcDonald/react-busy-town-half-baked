ab0d0c7ece16a8313675ec2bbefa798c7a7c1785
ClareMcDonald <clare.s.mcdonald@gmail.com>
2022-02-17 11:40:01 -0800

    button test passes

--- src/App.test.js
+++ src/App.test.js
@@ -1,10 +1,18 @@
 import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-test('renders an incremented count on setAlienSize click', () => {
+// test('renders an incremented count on setAlienSize click', () => {
+//   render(<App />);
+
+//   const alienSizeEl = screen.getby
+//   const linkElement = screen.getByText(/Yegads! The lizard is ramping up to its final form!/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+test('renders buttons in App.js', () => {
   render(<App />);
 
-  const alienSizeEl = screen.getby
-  const linkElement = screen.getByText(/Yegads! The lizard is ramping up to its final form!/i);
-  expect(linkElement).toBeInTheDocument();
+  const buttonArray = screen.getAllByRole('button');
+  expect(buttonArray.length).toBe(11);
+  
 });
