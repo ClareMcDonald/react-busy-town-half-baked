8e19e365eee6851e1f8f04fb436b97e8a253b34c
ClareMcDonald <clare.s.mcdonald@gmail.com>
2022-02-17 11:46:54 -0800

    simple alienButtonEl test passes

--- src/App.test.js
+++ src/App.test.js
@@ -1,18 +1,18 @@
 import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
-// test('renders an incremented count on setAlienSize click', () => {
-//   render(<App />);
-
-//   const alienSizeEl = screen.getby
-//   const linkElement = screen.getByText(/Yegads! The lizard is ramping up to its final form!/i);
-//   expect(linkElement).toBeInTheDocument();
-// });
-
 test('renders buttons in App.js', () => {
   render(<App />);
 
   const buttonArray = screen.getAllByRole('button');
   expect(buttonArray.length).toBe(11);
   
+});
+
+test('renders an incremented alien size on setAlienSize click', () => {
+  render(<App />);
+
+  const alienButtonEl = screen.getByText(/Oh no! The alien is gobblin up all the electricity!/i);
+
+  expect(alienButtonEl).toBeInTheDocument();
 });
